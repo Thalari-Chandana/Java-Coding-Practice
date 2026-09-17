5c9f0315ee8272828f71581423e1dd98225ffe16
Thalari-Chandana <thalarichandana6@gmail.com>
2026-09-17 18:52:09 +0530

    Reversing an Array

---
+++ Arrays/RevArray.java
@@ -0,0 +1,20 @@
+
+
+class RevArray {
+	public static void main(String[] args) {
+		int[] a= {1,2,3,4,5};
+		int start=0;
+		int end=a.length-1;
+		while(start<end) {
+			int temp=a[start];
+			a[start]=a[end];
+			a[end]=temp;
+			start ++;
+			end --;
+		}
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
